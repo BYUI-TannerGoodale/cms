ab247fb755abe959cc0f35e437a5c43741620289
Tanner Goodale <goo18011@byui.edu>
2022-01-28 11:56:13 -0600

    week 04 additionas added and tested

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [new Document(1, 'Logs', 'Recent log data', '#', null),
+                           new Document(2, 'Taxes', 'A list of taxes to be payed', '#', null),
+                           new Document(3, 'Name', 'Pointless filler data', '#', null),
+                           new Document(4, 'Test', 'This is test data', '#', null)];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
+  }
 }
